bc45dbd45502d80ba4fe66d4458a312020803154
Josh Grzech <joshuagrzech@gmail.com>
2021-08-17 14:25:22 -0500

    stable

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //A new GraphQL Apollo Server with Google Cloud Functions
 // https://github.com/apollographql/apollo-server/tree/main/packages/apollo-server-cloud-functions
 
-const { ApolloServer } = require('apollo-server');
+const { ApolloServer } = require('apollo-server-cloud-functions');
 const {typeDefs} = require('./typeDefs');
 const {resolvers} = require('./resolvers');
 
@@ -11,10 +11,4 @@ const server = new ApolloServer({
   introspection: true,
 });
 
-server.listen().then(() => {
-  console.log(`
-    Server is running!
-    Listening on port 4000
-    Explore at https://studio.apollographql.com/sandbox
-  `);
-});
+exports.handler = server.createHandler();
